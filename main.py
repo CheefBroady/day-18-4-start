ed2c23e58eb7ac2e32da70f293bce47c4cc040f0
Roland Bergauer <wi20m051@technikum-wien.at>
2021-08-08 23:21:03 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+import turtle as t
+from turtle import Screen
+import random
+
+tim = t.Turtle()
+
+########### Challenge 4 - Random Walk ########
+colours = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue", "LightSeaGreen", "wheat", "SlateGray",
+           "SeaGreen"]
+
+
+tim.forward(150)
+tim.color(random(colours))
