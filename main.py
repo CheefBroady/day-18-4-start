80f396bd7dc99479e45b8b2bc17dac44b8b088db
Uni_Roland <roland.bergauer@univie.ac.at>
2021-08-10 18:11:40 +0200

    auf Uni etwas hinzugefügt

--- main.py
+++ main.py
@@ -16,13 +16,17 @@ direction = [tim.forward(15), tim.left(15), tim.right(15)]
 
 while walk:
     tim.color(colours[rand.randint(0, 10)])
-    rand.random(direction)
+    # rand.random(direction)
     # tim.speed(speed="slow")
     # random(direction)
     counter += 1
     if counter == 15:
         walk = False
 
-
+# do anything this evening, 10. August 2021
+for n in range(1, 10):
+    print(f"Was geht zum {n}ten mal ab?")
+    print("und so weiter")
+    print("und da geht noch einer....")
 
 screen.exitonclick()
